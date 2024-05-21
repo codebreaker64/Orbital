eff5acff344a5557780b179d251fbb8bc307995e
codebreaker64 <64005011+codebreaker64@users.noreply.github.com>
2024-05-21 18:46:28 +0800

    Added const to MyApp()

--- orbital/lib/main.dart
+++ orbital/lib/main.dart
@@ -6,7 +6,7 @@ const supabaseKey = String.fromEnvironment('SUPABASE_KEY');
 
 Future<void> main() async {
   await Supabase.initialize(url: supabaseUrl, anonKey: supabaseKey);
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
