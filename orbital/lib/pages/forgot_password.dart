3e9ee7696ee0f6d713650465b9b789ca16c2092c
codebreaker64 <64005011+codebreaker64@users.noreply.github.com>
2024-05-23 03:26:02 +0800

    Design the ui for forgot_password and code the theme for all the pages in peacepal

--- orbital/lib/pages/forgot_password.dart
+++ orbital/lib/pages/forgot_password.dart
@@ -1,10 +1,57 @@
 import 'package:flutter/material.dart';
 
-class ForgotPassword extends StatelessWidget {
+class ForgotPassword extends StatefulWidget {
   const ForgotPassword({super.key});
 
   @override
+  State<ForgotPassword> createState() => _ForgotPasswordState();
+}
+
+class _ForgotPasswordState extends State<ForgotPassword> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Form(
+      key: _formKey,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+             "Forgot Password?",
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30.0)),
+          Text("Type your email to reset your password."),
+          Center(
+            child: Container(
+              width: 300,
+              child: TextFormField(
+                decoration: const InputDecoration(
+                  hintText: 'Enter your email',
+                ),
+                validator: (String? value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter some text';
+                  }
+                  return null;
+                },
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child: ElevatedButton(
+              onPressed: () {
+                // Validate will return true if the form is valid, or false if
+                // the form is invalid.
+                if (_formKey.currentState!.validate()) {
+                  // Process data.
+                }
+              },
+              child: const Text('Confirm'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
