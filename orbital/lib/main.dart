aa958652fd3a2b7c1b579aac59115e514d3242b9
codebreaker64 <64005011+codebreaker64@users.noreply.github.com>
2024-05-23 15:38:20 +0800

    Created user interface for updated password page.

--- orbital/lib/main.dart
+++ orbital/lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         )
       ),
       home: Scaffold(
-        body: const ForgotPassword(),
+        body: const PasswordUpdated(),
       ),
     );
   }
