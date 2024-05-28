9c8b476cff4436e3b5ffcd08b61fb2893728f360
Alastair Ng <yusenng88@gmail.com>
2024-05-28 19:11:32 +0800

    launch url for all article

--- orbital/lib/pages/dashboard.dart
+++ orbital/lib/pages/dashboard.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:http/http.dart' as http;
+import 'package:url_launcher/url_launcher.dart';
 
 class Dashboard extends StatefulWidget {
   @override
@@ -20,80 +20,181 @@ class _DashboardState extends State<Dashboard> {
 
   @override
   Widget build(BuildContext context) {
+    //first article link
+    final Uri s = Uri.parse(
+        "https://www.fulcrumfitness.com/blog/10-benefits-of-going-to-the-gym");
+    Future<void> _launchURL() async {
+      if (!await launchUrl(s)) {
+        throw Exception('Could not launch $s');
+      }
+    }
+
+//second article link
+    final Uri s1 = Uri.parse(
+        "https://www.medicalnewstoday.com/articles/154543#definition");
+    Future<void> _launchURL1() async {
+      if (!await launchUrl(s1)) {
+        throw Exception('Could not launch $s1');
+      }
+    }
+
+    //third article link:
+    final Uri s2 = Uri.parse(
+        "https://www.channelnewsasia.com/singapore/singapore-struggle-mental-health-issues-stress-emotions-work-studies-wellness-3883091");
+    Future<void> _launchURL2() async {
+      if (!await launchUrl(s2)) {
+        throw Exception('Could not launch $s2');
+      }
+    }
+
     return Scaffold(
       body: Center(
-        child: Row(
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(top: 70.0, left: 15.0),
-                    child: Text(
-                      'Welcome back,\nHow are you feeling today?',
-                      style: TextStyle(
-                          fontSize: 20.0, fontWeight: FontWeight.bold),
-                    ),
+            Stack(
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(
+                      left: 300,
+                      top: 50), // Match this value to align with the Text
+                  child: CircleAvatar(
+                    radius: 40,
+                    backgroundColor: Colors.red,
                   ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 50.0),
-                    child: Center(
-                      child: CarouselSlider(
-                        items: imagePaths.map((path) {
-                          return Container(
-                            margin: EdgeInsets.all(5.0),
-                            child: ClipRRect(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(5.0)),
-                              child: Image.asset(
-                                path,
-                                fit: BoxFit.cover,
-                                width: 1000.0,
-                              ),
-                            ),
-                          );
-                        }).toList(),
-                        options: CarouselOptions(
-                          height: 200.0,
-                          enlargeCenterPage: true,
-                          autoPlay: true,
-                          aspectRatio: 16 / 9,
-                          autoPlayCurve: Curves.fastOutSlowIn,
-                          autoPlayAnimationDuration:
-                              Duration(milliseconds: 800),
-                          enableInfiniteScroll: true,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 70.0, left: 15.0),
+                  child: Text(
+                    'Welcome back,\nHow are you feeling today?',
+                    style:
+                        TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 50.0),
+              child: Center(
+                child: CarouselSlider(
+                  items: imagePaths.map((path) {
+                    return Container(
+                      margin: EdgeInsets.all(5.0),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                        child: Image.asset(
+                          path,
+                          fit: BoxFit.cover,
+                          width: 1000.0,
                         ),
                       ),
+                    );
+                  }).toList(),
+                  options: CarouselOptions(
+                    height: 200.0,
+                    enlargeCenterPage: true,
+                    autoPlay: true,
+                    aspectRatio: 16 / 9,
+                    autoPlayCurve: Curves.fastOutSlowIn,
+                    autoPlayAnimationDuration: Duration(milliseconds: 800),
+                    enableInfiniteScroll: true,
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              child: Stack(
+                children: [
+                  Container(
+                    width: double.infinity,
+                    height: double.infinity,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.vertical(
+                        top: Radius.circular(40.0),
+                      ),
                     ),
                   ),
-                  Expanded(
-                      child: Stack(
-                    // padding: const EdgeInsets.only(top: 70.0, left: 15.0),
-                    children: [
-                      Container(
-                        width: double.infinity,
-                        height: double.infinity,
-                        color: Colors.blue,
-                      ),
-                      Container(
-                        width: 100,
-                        height: 200,
-                        color: Colors.white,
-                      ),
-                    ],
-                  ))
+                  Positioned(
+                    top: 50.0, // Adjust the top position
+                    left: 30.0, // Adjust the left position
+                    width: 100, // Adjust the width of the image
+                    height: 70.0, // Adjust the height of the image
+                    child: Image.asset(
+                      'images/uplift.jpg',
+                      fit: BoxFit
+                          .cover, // Adjust how the image should fit within the space
+                    ),
+                  ),
+                  Positioned(
+                      top: 50,
+                      left: 150,
+                      child: TextButton(
+                        onPressed: () {
+                          // Define the action to be performed on button press
+                          _launchURL();
+                        },
+                        child: Text(
+                          '10 Benefits of Going to\n the Gym',
+                          style: TextStyle(fontSize: 16.0, color: Colors.black),
+                        ),
+                      )),
+
+                  //second section of the article
+                  Positioned(
+                    top: 150.0, // Adjust the top position
+                    left: 30.0, // Adjust the left position
+                    width: 100, // Adjust the width of the image
+                    height: 70.0, // Adjust the height of the image
+                    child: Image.asset(
+                      'images/uplift.jpg',
+                      fit: BoxFit
+                          .cover, // Adjust how the image should fit within the space
+                    ),
+                  ),
+                  Positioned(
+                      top: 150,
+                      left: 150,
+                      child: TextButton(
+                        onPressed: () {
+                          // Define the action to be performed on button press
+                          _launchURL1();
+                        },
+                        child: Text(
+                          'What is mental health?',
+                          style: TextStyle(fontSize: 16.0, color: Colors.black),
+                        ),
+                      )),
+
+                  //third section of the article
+                  Positioned(
+                    top: 250.0, // Adjust the top position
+                    left: 30.0, // Adjust the left position
+                    width: 100, // Adjust the width of the image
+                    height: 70.0, // Adjust the height of the image
+                    child: Image.asset(
+                      'images/uplift.jpg',
+                      fit: BoxFit
+                          .cover, // Adjust how the image should fit within the space
+                    ),
+                  ),
+                  Positioned(
+                      top: 240,
+                      left: 150,
+                      child: TextButton(
+                        onPressed: () {
+                          // Define the action to be performed on button press
+                          _launchURL2();
+                        },
+                        child: Text(
+                          'Why do people in Singapore \nstruggle with mental health\nissue?',
+                          style: TextStyle(fontSize: 16.0, color: Colors.black),
+                        ),
+                      )),
                 ],
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.only(top: 50.0, right: 10),
-              child: CircleAvatar(
-                radius: 50,
-              ),
-            )
           ],
         ),
       ),
