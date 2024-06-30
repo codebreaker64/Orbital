71a65b59686f7e3ed4b1a951ee0fe6909ea3ad4b
codebreaker64 <64005011+codebreaker64@users.noreply.github.com>
2024-07-01 02:27:58 +0800

    Update moodtracker_test.dart

--- orbital/test/moodtracker_test.dart
+++ orbital/test/moodtracker_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import '../lib/Journal/moodtracker.dart';
+import 'package:orbital/Journal/moodTracker.dart';
 
 void main() {
   testWidgets('Moodtracker widget test', (WidgetTester tester) async {
